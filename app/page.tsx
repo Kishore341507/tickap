2fc5f1fe5075efe0bd76383406e8937d0e489530
kishore341507 <choudhary341507@gmail.com>
2024-08-04 20:55:42 +0530

    update nav and side bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import { ModeToggle } from "./mode-toggle";
+import { ModeToggle } from "../components/ui/mode-toggle";
 
 export default function Home() {
   return (
     <main>
-      <Button>Click me</Button>
-      <ModeToggle />
     </main>
   );
 }
