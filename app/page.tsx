72fa9e088ece724c58fd95b930b2bfdde42bdc32
kishore341507 <choudhary341507@gmail.com>
2024-08-04 15:09:42 +0530

    intigrate shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { ModeToggle } from "./mode-toggle";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="" >Hello World</h1>
-      <Link href="/users"> Go to users</Link>
+      <Button>Click me</Button>
+      <ModeToggle />
     </main>
   );
 }
