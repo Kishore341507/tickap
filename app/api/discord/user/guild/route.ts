2a7d2d5e3df4eefd760c9ad9918425784a88ea97
kishore341507 <choudhary341507@gmail.com>
2024-08-11 02:36:53 +0530

    fir guild

--- app/api/discord/user/guild/route.ts
+++ app/api/discord/user/guild/route.ts
@@ -4,24 +4,24 @@ import { NextRequest, NextResponse } from "next/server";
 import { env } from "process";
 
 export async function GET( request : NextRequest ) {
-    const session = await auth() ;
-    if (!session) {
-        return NextResponse.json("Unauthorized" , { status : 401 } );
-    }
+    // const session = await auth() ;
+    // if (!session) {
+    //     return NextResponse.json("Unauthorized" , { status : 401 } );
+    // }
 
-    const accessToken = (await prisma.account.findFirst({ where : { userId : (await auth())?.user?.id } }))?.access_token
-    const userGuildsResponce : Response = await fetch(env.DISCORD_API_URL + '/users/@me/guilds' , {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        } , next : {
-            revalidate : 600
-        }
-    });
-    const userGuilds = await userGuildsResponce.json();
-    userGuilds.forEach((guild : any) => {
-        guild.icon = guild.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png` : 'https://cdn.discordapp.com/embed/avatars/0.png';
-        guild.manager = (BigInt(guild.permissions) & BigInt(0x20)) !== BigInt(0);
-    });
+    // const accessToken = (await prisma.account.findFirst({ where : { userId : (await auth())?.user?.id } }))?.access_token
+    // const userGuildsResponce : Response = await fetch(env.DISCORD_API_URL + '/users/@me/guilds' , {
+    //     headers: {
+    //         Authorization: `Bearer ${accessToken}`
+    //     } , next : {
+    //         revalidate : 600
+    //     }
+    // });
+    // const userGuilds = await userGuildsResponce.json();
+    // userGuilds.forEach((guild : any) => {
+    //     guild.icon = guild.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png` : 'https://cdn.discordapp.com/embed/avatars/0.png';
+    //     guild.manager = (BigInt(guild.permissions) & BigInt(0x20)) !== BigInt(0);
+    // });
 
-    return NextResponse.json(userGuilds);
+    return NextResponse.json({ message : "Hello World" });
 }
