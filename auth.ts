5679bb5d68ee1200d167bbf2db295403387ad85b
kishore341507 <choudhary341507@gmail.com>
2025-05-10 23:13:22 +0530

    feat : add user to server if not in server

--- auth.ts
+++ auth.ts
@@ -19,7 +19,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         Discord({
             clientId: process.env.DISCORD_CLIENT_ID,
             clientSecret: process.env.DISCORD_CLIENT_SECRET,
-            authorization: "https://discord.com/oauth2/authorize?scope=identify%20guilds%20email&response_type=code&prompt=consent",
+            authorization: "https://discord.com/oauth2/authorize?scope=identify%20guilds%20email%20guilds.join&response_type=code&prompt=consent",
         })
     ],
     callbacks: {
