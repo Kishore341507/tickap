d1e48b3e99e5e894d569be24217322d308cba529
Kamal Kishore <choudhary341507@gmail.com>
2026-01-31 15:43:32 +0000

    feat: remove tickap.com remote pattern from image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,6 @@ const nextConfig = {
             port: '',
             pathname: '/**',
           },
-          {
-            protocol: 'https',
-            hostname: 'tickap.com',
-            port: '',
-            pathname: '/**',
-          },
         ],
     },
     logging : {
